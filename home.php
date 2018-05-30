735b0c8dfff719115a056cd241a99faadeed7d98
Emma Grdina <emmagrdina@gmail.com>
2018-05-29 20:30:32 -0400

    Older posts and next posts CSS
    Styling the Read more and Older posts blog links

--- home.php
+++ home.php
@@ -20,7 +20,6 @@ get_header(); ?>
 
 	<?php endwhile; ?>
 
-    <div class="nav-previous alignleft"><?php next_posts_link('Older posts'); ?></div>
-    <div class="nav-next alignright"><?php previous_posts_link('Newer posts'); ?></div>
+    <div class="next-posts"><?php next_posts_link('Older posts'); ?></div>
 
 <?php get_footer(); ?>

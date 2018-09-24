7d8ea7af550ef81a85937d11098daf6810dc82a2
Emma Grdina <emmagrdina@gmail.com>
2018-09-24 18:23:44 -0400

    values section and footer color changes

--- page-services.php
+++ page-services.php
@@ -11,36 +11,53 @@
 </div>
 
 <div class="content">
-    <p>My process is rooted in the belief that design should be delightful and easy to consume whether you're reading a website, instagramming an event's decorations or receiving an invite in the mail.</p>
+    <p>I’m a firm believer that sophisticated, minimal design and striking typography lead to delightful experiences both online and IRL.</p>
 
-    <p>Whether you’re a bride or groom planning your wedding and in need of an invitation suite or a small business owner looking to spruce up your online presence, I’d love to work with you.</p>
+    <p>Every project has its own unique set of needs, but my values set me apart as a dedicated designer, passionate communicator and empathetic human.</p>
 </div> 
+
+<div class='values'>
+    <h1>Values</h1>
+    <ul id="one"><i class="fas fa-hand-holding-heart"></i><p>Collaboration</p> 
+        <p>Before I create anything, I like to learn about your goals and how you define success. Your motivations inform my design decisions and the overall strategy.</p>
+    </ul>
+    <ul id="two"><i class="fas fa-mobile-alt"></i><p>Communication</p>
+        <p>I believe there is a time and a place for surprises, and your project is not one of them. As a client, you will receive regular updates and have the opportunity to provide feedback throughout our time working together.</p>
+    </ul>
+    <ul id="three"><i class="fas fa-heart"></i><p>Intention</p>
+        <p> Designs created with a purpose are not only easy to consume but they radiate energy and excitement. Audiences can tell when something has intention behind it, and I am to infuse meaning into everything I make.</p>
+    </ul>
+</div>
+
 
 <div class="middle-section">
     <h5>Services</h5>
-    <h1>Web and mobile design</h1>
+    <h1>Web design</h1>    
+    <h1>Brand packages</h1>
     <h1>Invitation suites</h1>
-    <h1>Logos and brand packages</h1>
-    <h1>Event signage</h1>
 </div>
 
 <div class="faq">
     <h1>FAQ</h1>
     <div class="content">
-        <h3>I know what I need, but I don’t see it listed above. Can we still work together?</h3> 
-        <p>My list of services is meant to provide a general idea of my capabilities, but the packages I offer are customized for every client’s individual needs.</p>
-
-        <p>No matter the project, I like to start by learning more about you, your goals and design needs. Then I  create several package options. After you select which package best meets your needs, I require a 25% deposit before starting a project.</p> 
+        <h3>I know what I need, but I don’t see it listed above. Can we still work together?</h3>
+        <p>My list of services provides a general idea of my capabilities, but the packages I offer are adjusted for every client’s individual needs. I’m a digital product designer by trade, and I have experience ranging from designing invitation suites and social media graphics to custom, multi-page, responsive websites.</p> 
+        <p>Whether you’re a small business who wants to spruce up their online presence or you’re launching a passion project, I’d love to work with you.</p> 
 
         <h3>I’m not entirely sure what I need, but I have a general idea. What should I do?</h3>
-        <p>Email is the best way to reach me. Send questions or just a hello to emmagrdina@gmail.com.</p>
+        <p>Email me! You can always find me at emmagrdina@gmail.com.</p>
 
         <p>It would be helpful for me to know any ideas you have and what your deadline might be. That’s it! Then I will reach out to you about potential next steps.</p> 
 
-        <h3>What if my project has a quick turnaround?</h3>
-        <p>Every project has a varied timeline. Some factors include but are not limited to what you need, when you need it by and what other projects I'm working on.</p>
+        <h3>I sent you an email. Now what?</h3> 
+        <p>I try to respond to emails within 48 hours of receiving them. My first step is always to learn more about you, your goals and design needs, ideally over the phone if the stars align. Phone calls give me the opportunity to get a quick sense of where you are in your own timeline and how we might collaborate.</p> 
 
-        <p>Designing in a newsroom taught me not only how to work quickly but also prioritize ruthlessly. I aim for no surprises throughout my design process regardless of the timeline. Communication and collaboration are key to your success and happiness and mine.</p>
+        <p>After I learn more about you and your project, then I create several package options. When you select which package best meets your needs, I require a non-refundable 50% deposit before starting a project.</p> 
+
+        <h3>What if my project has a quick turnaround?</h3>
+        <p>Designing in a newsroom taught me not only how to work quickly but also how to prioritize ruthlessly. Every project has a timeline which is tailored to its unique considerations. Some factors include but are not limited to designs you need, when your deadline is and my current workload.</p>
+
+        <p>I aim for no surprises throughout my design process regardless of the timeline. Communication and collaboration are key to your success and happiness and mine.</p>
     </div> 
 </div>
 

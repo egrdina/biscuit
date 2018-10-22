f296b5cebd028a47c0d31cf0ad78f0df84b0bad8
Emma Grdina <emmagrdina@gmail.com>
2018-10-22 16:19:14 -0400

    never-ending font adjustments

--- page-services.php
+++ page-services.php
@@ -44,26 +44,26 @@
 <div class="faq">
     <h1>FAQ</h1>
     <div class="content">
-        <h3>I know what I need, but I don’t see it listed above. Can we still work together?</h3>
+        <p class="question">I know what I need, but I don’t see it listed above. Can we still work together?</p>
         <p>My list of services provides a general idea of my capabilities, but the packages I offer are adjusted for every client’s individual needs. I’m a digital product designer by trade, and I have experience ranging from designing invitation suites and social media graphics to custom, multi-page, responsive websites.</p> 
         <p>Whether you’re a small business who wants to spruce up their online presence or you’re launching a passion project, I’d love to work with you.</p> 
 
-        <h3>I’m not entirely sure what I need, but I have a general idea. What should I do?</h3>
+        <p class="question">I’m not entirely sure what I need, but I have a general idea. What should I do?</p>
         <p>Email me at emmagrdina@gmail.com.</p>
 
         <p>It would be helpful for me to know any ideas you have and what your deadline might be. That’s it! Then I will reach out to you about potential next steps.</p> 
 
-        <h3>I sent you an email. Now what?</h3> 
+        <p class="question">I sent you an email. Now what?</p> 
         <p>I try to respond to emails within 48 hours of receiving them. Then my next step is learning more about you, your goals and design needs, ideally over the phone if the stars align. Phone calls give me the opportunity to get a quick sense of where you are in your own timeline and how we might collaborate.</p> 
 
         <p>After I learn more about you and your project, then I create several package options to provide you with a range of design assets at various price points. When you select which package best meets your needs, I require a non-refundable 50% deposit before starting a project.</p> 
 
-        <h3>What if my project has a quick turnaround?</h3>
+        <p class="question">What if my project has a quick turnaround?</p>
         <p>Designing in a newsroom taught me not only how to work quickly but also how to prioritize ruthlessly. Every project has a timeline tailored to its unique considerations. Some factors include but are not limited to designs you need, the deadline and my current workload.</p>
 
         <p>Communication and collaboration starting on day one of our working together are key to your success and happiness and mine.</p>
 
-        <h3>Not sure where to start?</h3>
+        <p class="question">Not sure where to start?</p>
         <p>Whether you have an existing visual identity or you're looking to spruce up your digital footprint, I offer free 15-min. UX and design consultations. During these calls we'll walk through your existing design assets (website, social graphics, etc.) and I'll share where I see opportunities for improvement.</p>
         <p>Then it's up to you to decide if you'd like to keep the conversation going! I know design can feel overwhelming. Let me help you make sense of it all.</p>
 
